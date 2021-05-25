89bfb15b724f3a5584d47f11f0e82e5b8cdd28f0
Hamza Kassomou <hazkassb@gmail.com>
2021-05-25 17:00:50 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,24 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: String | undefined;
+  @Input() color: String | undefined;
+  @Output() btnClick = new EventEmitter
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.btnClick.emit();
+  }
+
+}
